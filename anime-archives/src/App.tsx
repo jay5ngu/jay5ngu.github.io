b72baf19659f9400656604d34505a9f3bd566cd3
Jayson Nguyen <jayngu357@gmail.com>
2026-08-07 16:14:26 -0700

    Starter code for anime archive frontend

---
+++ anime-archives/src/App.tsx
@@ -0,0 +1,75 @@
+import { useState } from 'react'
+import './css/App.css'
+import './css/book.css'
+import './css/shelf.css'
+
+// Sample Backend Data (REMOVE LATER)
+import animeList from './api/sample-data/myShows.json'
+
+function App() {
+  // Anime selection and shelf
+  const [selectedAnime, setSelectedAnime] = useState(animeList[0])
+  const [selectedId, setSelectedId] = useState(animeList[0].id)
+  const [myAnimeList, setMyAnimeList] = useState(animeList)
+
+  // For open book animation
+  const [isOpen, setIsOpen] = useState(false)
+
+  return (
+    <>
+      <section id="center">
+        {/* Website Title */}
+        <div>
+          <h1>Anime Archives</h1>
+          <p>{myAnimeList.length} titles on the shelf</p>
+        </div>
+
+        {/* Selected Anime/Show Book Info */}
+        <div className="bookStage">
+          {/* Content inside the book */}
+          <div className="book">
+            <div className="anime">
+              <div className='leftPage'>
+                <img src={selectedAnime.coverImage.large} alt={selectedAnime.title.english} />
+              </div>
+              <div className='rightPage'>
+                <h2>{selectedAnime.title.english}</h2>
+                <p className='meta'>Released {selectedAnime.startDate}</p>
+                <p className='synopsis'>{selectedAnime.description}</p>
+              </div>
+            </div>
+
+            {/* Front of Page with open/close functionality */}
+            <div className="cover" style={{ transform: isOpen ? 'rotateY(-150deg) translateZ(0)' : 'rotateY(0deg) translateZ(0)' }} onClick={() => setIsOpen(!isOpen)}>
+              <div className="coverFront">
+                <h1>{selectedAnime.title.english}</h1>
+                <p>click to open</p>
+              </div>
+              {/* Click this div to close book */}
+              <div className="coverBack"></div>
+            </div>
+          </div>
+        </div>
+
+        {/* Shelf Anime/Show Selection */}
+        <div className='shelf'>
+          {myAnimeList.map((show) => (
+            <section key={show.id} className={show.id === selectedId ? "active" : ""}>
+              <button onClick={() => {
+                setSelectedAnime(show)
+                setSelectedId(show.id)
+                // setIsOpen(false)
+              }}>
+                <img src={show.coverImage.medium} alt={show.title.english} />
+                {show.title.english}
+              </button>
+            </section>
+          ))}
+        </div>
+      </section>
+
+    </>
+  )
+}
+
+export default App
---
+++ anime-archives/src/types/shows.ts
@@ -0,0 +1,21 @@
+// Set data structure for shows
+interface title {
+  romaji: string;
+  english: string;
+  native: string;
+}
+
+interface coverImage {
+    medium: string;
+    large: string;
+    extraLarge: string;
+    color: string
+}
+export interface Show {
+  id: number;
+  title: title;
+  // kind: "Anime" | "TV";
+  description: string;
+  startDate: string;
+  coverImage: coverImage;
+}
